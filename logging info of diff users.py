9c2f11577380bce7bbe029f4e629b7d1f4bc5ccf
karthiksamala03 <77474773+karthiksamala03@users.noreply.github.com>
2022-07-02 20:37:30 +0530

    Update logging info of diff users.py
    Updated the code

--- logging info of diff users.py
+++ logging info of diff users.py
@@ -2,14 +2,18 @@ import logging
 
 logging.basicConfig(filename='logging info of diff users.log', level=logging.DEBUG, format='%(levelname)s %(asctime)s %(name)s : %(message)s')
 
+# Create Handlers
 consol_log = logging.StreamHandler()
-consol_log.setLevel(logging.INFO)
-format = '%(levelname)s %(asctime)s %(name)s : %(message)s'
+consol_log.setLevel(logging.DEBUG)
+format = logging.Formatter('%(levelname)s %(asctime)s %(name)s : %(message)s')
 consol_log.setFormatter(format)
+
+# Creating a Custom Logger
 logging.getLogger().addHandler(consol_log)
-logging.info("logging of code for multiple users")
-logger1 = logging.getLogger('logger1.area1')
-logger2 = logging.getLogger('logger2.area2')
-logger1.info("This is info for logger1")
-logger1.debug("This is debug for logger1")
-logger2.info("This is info for logger2")
+logging.info("user Root -  main log")
+logger1 = logging.getLogger('User1')
+logger2 = logging.getLogger('User2')
+logger1.info("This is info for USER1")
+logger1.debug("This is debug for USER1")
+logger2.info("This is info for USER2")
+
